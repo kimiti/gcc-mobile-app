ebe3cee2230a25300f259ed0938acd7985ea1bf6
kimiti <kimitikevin@gmail.com>
2021-10-19 09:26:36 +0300

    adds login page

---
+++ app/src/main/java/com/gracecommunitycenter/gracecommunitycenter/data/Repository.kt
@@ -0,0 +1,5 @@
+package com.gracecommunitycenter.gracecommunitycenter.data
+
+class Repository {
+
+}
---
+++ app/src/main/java/com/gracecommunitycenter/gracecommunitycenter/data/firebase/auth/FirebaseAuth.kt
@@ -0,0 +1,4 @@
+package com.gracecommunitycenter.gracecommunitycenter.data.firebase.auth
+
+class FirebaseAuth {
+}
