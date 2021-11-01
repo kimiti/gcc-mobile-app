80e620b5c2223051ad8bd2fe4386ea546884c3c5
kimiti <kimitikevin@gmail.com>
2021-11-01 10:35:44 +0300

    code cleanup

--- app/src/main/java/com/gracecommunitycenter/gracecommunitycenter/fragments/auth/AuthLoginFragment.kt
+++ app/src/main/java/com/gracecommunitycenter/gracecommunitycenter/fragments/auth/AuthLoginFragment.kt
@@ -7,11 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
